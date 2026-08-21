53f622173ee81d6ef3daec527e3933ed37a59618
Norman Power <NormyRoe@hotmail.com>
2026-08-21 12:31:28 +1000

    Problem 2 and 3: Remove unused imports from Problem 2 and started setting up Problem 3.

--- src/problem_3.py
+++ src/problem_3.py
@@ -1,28 +1,44 @@
 
-"""
-main.py
-Entry point for the assignment.
 
-This template includes standard imports (numpy, matplotlib)
-so you can begin coding immediately.
-"""
-
-import numpy as np
-import matplotlib.pyplot as plt
+###############################################################################################
+# Imports
+#
+###############################################################################################
+import math
+import time
+import tracemalloc
 
 
+
+
+
+
+
+
+
+
+
+
+###############################################################################################
+# Function: problem_3
+# Description:
+# Entry Point for running Problem 3
+# 
+#
+# Input:    N/A
+# Output:   N/A
+###############################################################################################
 def problem_3():
-    print("Program started.")
-    # TODO: Add your assignment logic here
+    """
+        main.py
+        Entry point for the assignment.
 
-    # Example placeholder using numpy
-    example_array = np.array([1, 2, 3])
-    print("Example numpy array:", example_array)
-
-    # Example placeholder using matplotlib
-    # plt.plot(example_array)
-    # plt.show()
+        The function:
+            - 
+    """
+    print("Program started.\n")
 
 
-if __name__ == "__problem_3__":
+
+if __name__ == "__main__":
     problem_3()
