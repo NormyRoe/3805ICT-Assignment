3e1df0616f14fcc784d388207edc84078515469e
Norman Power <NormyRoe@hotmail.com>
2026-08-22 15:03:19 +1000

    Problem 3: Removed a test case and performed testing.

--- tests/test_problem_3.py
+++ tests/test_problem_3.py
@@ -114,8 +114,8 @@ def test_case_4_score_under_75():
     print("Test Case 4: Similarity Score under 75%\n")
 
     # Create authorized voice and test voice time series
-    voice_authorized = [10, 20, 30, 40, 50]
-    voice_test = [10, 11, 12, 13, 14]
+    voice_authorized = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210]
+    voice_test = [11, 12, 20, 38, 56, 58, 60, 70, 72]
 
     # Perform SAX
     perform_sax(voice_authorized, voice_test)
@@ -145,8 +145,8 @@ def test_case_5_score_over_75():
     print("Test Case 5: Similarity Score over 75%\n")
 
     # Create authorized voice and test voice time series
-    voice_authorized = [10, 20, 30, 40, 50]
-    voice_test = [10, 20, 40, 41, 42]
+    voice_authorized = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210]
+    voice_test = [10, 40, 70, 100, 115, 135, 150, 180, 210]
 
     # Perform SAX
     perform_sax(voice_authorized, voice_test)
@@ -163,52 +163,21 @@ def test_case_5_score_over_75():
 
 
 ###############################################################################################
-# Test Function: test_case_6_score_equals_75
-# Description:
-# Tests what happens when the Test Voice similarity score equals 75%
-#
-# Input:    N/A
-# Output:   N/A
-###############################################################################################
-def test_case_6_score_equals_75():
-
-    # Print what is being tested
-    print("Test Case 6: Similarity Score equals 75%\n")
-
-    # Create authorized voice and test voice time series
-    voice_authorized = [10, 20, 30, 40]
-    voice_test = [10, 20, 29, 31]
-
-    # Perform SAX
-    perform_sax(voice_authorized, voice_test)
-
-    # Print an empty line
-    print("\n")
-
-    # Print a separator line
-    print("=" * 80)
-
-    # Print an empty line
-    print("\n")
-
-
-
-###############################################################################################
-# Test Function: test_case_7_medium_inputs
+# Test Function: test_case_6_medium_inputs
 # Description:
 # Tests what happens when the Authorized Voice and the Test Voice series are medium sized
 #
 # Input:    N/A
 # Output:   N/A
 ###############################################################################################
-def test_case_7_medium_inputs():
+def test_case_6_medium_inputs():
 
     # Print what is being tested
-    print("Test Case 7: Authorized Voice and Test Voice inputs are medium sized\n")
+    print("Test Case 6: Authorized Voice and Test Voice inputs are medium sized\n")
 
     # Create authorized voice and test voice time series
-    voice_authorized = list(range(500, 900))
-    voice_test = list(range(550, 950))
+    voice_authorized = list(range(500, 1000))
+    voice_test = list(range(350, 750))
 
     # Perform SAX
     perform_sax(voice_authorized, voice_test)
@@ -225,21 +194,21 @@ def test_case_7_medium_inputs():
 
 
 ###############################################################################################
-# Test Function: test_case_8_large_inputs
+# Test Function: test_case_7_large_inputs
 # Description:
 # Tests what happens when the Authorized Voice and the Test Voice series are large
 #
 # Input:    N/A
 # Output:   N/A
 ###############################################################################################
-def test_case_8_large_inputs():
+def test_case_7_large_inputs():
 
     # Print what is being tested
-    print("Test Case 8: Authorized Voice and Test Voice inputs are large\n")
+    print("Test Case 7: Authorized Voice and Test Voice inputs are large\n")
 
     # Create authorized voice and test voice time series
-    voice_authorized = list(range(1000, 2500))
-    voice_test = list(range(1505, 3005))
+    voice_authorized = list(range(1000, 4500))
+    voice_test = list(range(2505, 4005))
 
     # Perform SAX
     perform_sax(voice_authorized, voice_test)
@@ -262,6 +231,5 @@ if __name__ == "__main__":
     test_case_3_paa_fail_test()
     test_case_4_score_under_75()
     test_case_5_score_over_75()
-    test_case_6_score_equals_75()
-    test_case_7_medium_inputs()
-    test_case_8_large_inputs()
+    test_case_6_medium_inputs()
+    test_case_7_large_inputs()
